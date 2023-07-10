f6d64cb4bd99812e7adeb264096b4e436c812651
Martin Mroz <mamr@hotmail.no>
2023-07-10 17:50:03 +0200

    Global styles

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import LandingPage from './pages/LandingPage';
-
+import GlobalStyle from "./GlobalStyles";
 
 function App() {
   return (
+    <>
+    <GlobalStyle /> 
     <Router>
       <Routes>
         <Route path="/" element={<LandingPage />} />
-     
       </Routes>
     </Router>
+    </>
   );
 }
 
