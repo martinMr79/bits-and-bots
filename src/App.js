188bd9f368fc4b3aedb03b831e195673d5c12d2d
Martin Mroz <mamr@hotmail.no>
2023-07-12 16:52:12 +0200

    setting up page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import LandingPage from './pages/LandingPage';
+import Browse from './pages/Browse';
 import GlobalStyle from './GlobalStyles';
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element={<LandingPage />} />
+          <Route path="/browse" element={<Browse />} />
         </Routes>
       </Router>
     </>
---
+++ src/pages/Browse/index.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import useFetch from '../../hooks/useFetch';
+import { Link } from 'react-router-dom';
+
+function Browse() {
+  const {
+    data: games,
+    loading,
+    error,
+  } = useFetch('https://bit-and-bots.volumvekt.no/wp-json/wp/v2/media');
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error.message}</p>;
+
+  return (
+    <div>
+      {games.map((game) => (
+        <div key={game.id}>
+          <h2 dangerouslySetInnerHTML={{ __html: game.title.rendered }}></h2>
+          {game.media_details && game.media_details.sizes.full && (
+            <img
+              src={game.media_details.sizes.full.source_url}
+              alt={game.alt_text || 'game'}
+            />
+          )}
+          <Link to={`/details/${game.id}`}>More info</Link>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default Browse;
+
