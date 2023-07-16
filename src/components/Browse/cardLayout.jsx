549b92d0afc24bc05eb89c485bb665fc0b1fc862
Martin Mroz <mamr@hotmail.no>
2023-07-16 23:45:44 +0200

    adding card layout

---
+++ src/components/Browse/cardLayout.jsx
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+export const ImageGrid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+  gap: 20px;
+  justify-content: center;
+`;
