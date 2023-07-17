8dd9dd3b2c5a72aa59f0ec5dd401d3ec94d26b1c
Martin Mroz <mamr@hotmail.no>
2023-07-17 16:11:05 +0200

    card layout

--- src/components/Browse/cardLayout.jsx
+++ src/components/Browse/cardLayout.jsx
@@ -1,22 +1,35 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-  max-width: 1200px;
+  max-width: 1400px;
   margin: 0 auto;
-  padding: 20px;
+  padding: 1rem;
 `;
 
 export const ImageGrid = styled.div`
   display: grid;
-  grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
-  gap: 20px;
+  grid-template-columns: repeat(1, minmax(0, 1fr));
+  gap: 1rem;
   justify-content: center;
+
+  @media (min-width: 680px) {
+    grid-template-columns: repeat(2, minmax(0, 1fr));
+  }
+
+  @media (min-width: 1050px) {
+    grid-template-columns: repeat(3, minmax(0, 1fr));
+  }
+
+  @media (min-width: 1400px) {
+    grid-template-columns: repeat(4, minmax(0, 1fr));
+  }
+
 `;
 
 export const Card = styled.div`
   background-color: #27282E;
   color: #ffffff;
-  padding: 20px;
+  padding: 1rem;
 `;
 
 export const ProductImage = styled.img`
