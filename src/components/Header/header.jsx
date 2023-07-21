22b10bc6c7f8a3207ca0ea7769c142ab2f26493b
Martin Mroz <mamr@hotmail.no>
2023-07-21 16:24:28 +0200

    cartIcon counter

--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { AppBar, Toolbar as MuiToolbar, Typography, IconButton } from '@mui/material';
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 import { Avatar } from '@mui/material';
+import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 
 const StyledAppBar = styled(AppBar)`
@@ -25,13 +26,43 @@ const Toolbar = styled(MuiToolbar)`
 
 const ContentContainer = styled.div`
   display: flex;
-  justify-content: space-between;
   align-items: center;
   max-width: 1500px;
   width: 100%;
+  position: relative;
 `;
 
+const CartLink = styled(Link)`
+  display: flex;
+  align-items: center;
+  text-decoration: none;
+  color: inherit;
+`;
+
+const CartItemCount = styled.span`
+  background-color: #CCFF00;
+  color: black;
+  border-radius: 50%;
+  padding: 6px;
+  font-size: 12px;
+  font-weight: bold;
+  position: absolute;
+  top: -1px;
+  right: 5px;
+  width: 12px;
+  height: 12px;
+`;
+
+
 function Header() {
+  // Function to get the number of items in the cart from localStorage
+  const getCartItemCount = () => {
+    const cartData = JSON.parse(localStorage.getItem('cart')) || [];
+    return cartData.length;
+  };
+
+  const cartItemCount = getCartItemCount();
+
   return (
     <StyledAppBar position="static">
       <Toolbar>
@@ -39,10 +70,13 @@ function Header() {
           <Title variant="h5">
             Bits & Bots
           </Title>
-          <IconButton color="inherit" style= {{ marginRight: 30 }}>
-            <ShoppingCartIcon style={{ fontSize: 40, }} />
-          </IconButton>
-          <Avatar style={{ color: 'black', backgroundColor: 'white' }}>A</Avatar>
+          <CartLink to="/cart">
+            <IconButton color="inherit">
+              <ShoppingCartIcon style={{ fontSize: 40 }} />
+              {cartItemCount > 0 && <CartItemCount>{cartItemCount}</CartItemCount>}
+            </IconButton>
+          </CartLink>
+          <Avatar style={{ color: 'black', backgroundColor: 'white', marginLeft: '16px' }}>A</Avatar>
         </ContentContainer>
       </Toolbar>
     </StyledAppBar>
@@ -50,4 +84,8 @@ function Header() {
 }
 
 export default Header;
+
+
+
+
 
