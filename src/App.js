c8023bd1529901eab145221931e52265f26d95de
Martin Mroz <mamr@hotmail.no>
2023-07-24 20:44:11 +0200

    add to cart basic setup

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LandingPage from './pages/LandingPage';
 import Browse from './pages/Browse';
 import ProductDetails from './pages/Details';
+import Cart from './pages/Cart';
 import { GlobalStyle, AppContainer } from './GlobalStyles';
 import MainLayout from './components/MainLayout';
 import './GlobalStyles'; 
@@ -17,6 +18,7 @@ function App() {
           <Route path="/" element={<LandingPage />} />
           <Route path="/browse" element={<MainLayout><Browse /></MainLayout>} />
           <Route path="/details/:id" element={<MainLayout><ProductDetails /></MainLayout>} />
+          <Route path="/cart" element={<MainLayout><Cart /></MainLayout>} />
         </Routes>
       </Router>
     </AppContainer>
---
+++ src/pages/Cart/index.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { useCart } from '../../hooks/useCart';
+
+const Cart = () => {
+  const { cartItems, removeFromCart, addToCart } = useCart();
+
+  if (!cartItems || cartItems.length === 0) return <p>Your cart is empty.</p>;
+
+
+  return (
+    <div>
+      <h1>Your Cart</h1>
+      {cartItems.map((item) => (
+        <div key={item.id}>
+          <img src={item.images[0]?.src} alt={item.images[0]?.alt || 'product'} />
+          <h2>{item.name}</h2>
+          <p>Price: {item.price} Nok</p>
+          <button onClick={() => removeFromCart(item.id)}>Remove from Cart</button>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Cart;
