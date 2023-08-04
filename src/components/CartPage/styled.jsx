600974b194247a6711884e538ef5ec2be6ce73b1
Martin Mroz <mamr@hotmail.no>
2023-08-05 01:33:07 +0200

    adding summary box

--- src/components/CartPage/styled.jsx
+++ src/components/CartPage/styled.jsx
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-
+import { colors } from '../../styles/theme';
 
 export const CartPageContainer = styled.div`
   max-width: 1500px;
@@ -7,64 +7,98 @@ export const CartPageContainer = styled.div`
   padding: 1rem;
   padding-bottom: 8rem;
   min-height: 70vh;
+  display: flex;
+  flex-direction: column;
 
   h1 {
-    color: white; 
-    margin-top: 2rem; 
+    color: white;
+    margin-top: 2rem;
     margin-bottom: 3rem;
     font-size: 26px;
   }
 
-  > div { 
-    background-color: #27282E;
-    margin-bottom: 1rem; 
+  .cart-wrapper {
     display: flex;
-    flex-direction: column;
+    justify-content: space-between;
 
-    div.row {
+    .cart-content {
+      flex: 3; 
+      background-color: #27282E;
+      margin-bottom: 1rem;
       display: flex;
+      flex-direction: column;
 
-      img {
-        max-height: 130px;
-        flex-shrink: 0;
-        margin-right: 2rem;
-      }
-
-      div.info {
-        flex-grow: 1;
+      div.row {
         display: flex;
-        justify-content: space-between;
-        align-items: center;
-        margin-right: 1rem;
 
-        h2 {
-          margin: 0;
-          color: white; 
-          margin-right: 8rem; 
-          font-size: 26px;
+        img {
+          max-height: 130px;
+          flex-shrink: 0;
+          margin-right: 2rem;
         }
 
-        div.price-and-button {
+        div.info {
+          flex-grow: 1;
           display: flex;
-          flex-direction: column;
-          align-items: flex-end;
+          justify-content: space-between;
+          align-items: center;
+          margin-right: 1rem;
 
-          p {
+          h2 {
             margin: 0;
             color: white;
-            margin-bottom: 1rem;
+            margin-right: 26rem;
+            font-size: 26px;
           }
 
-          button {
-            background-color: #27282E;
-            color: #2177EB;
-            border: none;
+          div.price-and-button {
+            display: flex;
+            flex-direction: column;
+            align-items: flex-end;
+
+            p {
+              margin: 0;
+              color: white;
+              margin-bottom: 1rem;
+            }
+
+            button {
+              background-color: #27282E;
+              color: #2177EB;
+              border: none;
+            }
           }
         }
       }
     }
+
+    .summary-box {
+      flex: 1;
+      background-color: ${colors.primary};
+      padding: 1rem;
+      margin-left: 1rem;
+      border: 1px solid #ddd;
+
+      h2 {
+        /* Styling for Summary title */
+        color: ${colors.white};
+      }
+
+      .summary-row {
+        /* Styling for the summary rows */
+        color: ${colors.white};
+      }
+
+      button {
+        /* Styling for the checkout button */
+        background-color: ${colors.secondary};
+        color: ${colors.black};
+        border: none;
+      }
+    }
   }
 `;
+
 
 
 
--- src/pages/Cart/index.jsx
+++ src/pages/Cart/index.jsx
@@ -7,40 +7,67 @@ const Cart = () => {
 
   if (!cart || cart.length === 0) return <p>Your cart is empty.</p>;
 
+  let totalPrice = 0;
+  let totalDiscount = 0;
+
+  cart.forEach(item => {
+    if (item.sale_price) {
+      totalPrice += parseFloat(item.sale_price);
+      totalDiscount += parseFloat(item.regular_price) - parseFloat(item.sale_price);
+    } else {
+      totalPrice += parseFloat(item.price);
+    }
+  });
+
+  let finalPrice = totalPrice - totalDiscount;
+
   return (
     <CartPageContainer>
       <h1>Your Cart</h1>
-      {cart.map((item) => {
-  const onSale = item.sale_price;
-  return (
-    <div key={item.id}>
-      <div className="row">
-        <img src={item.images[0]?.src} alt={item.images[0]?.alt || 'product'} />
-        <div className="info">
-          <h2>{item.name}</h2>
-          <div className="price-and-button">
-            {onSale ? (
-              <p>
-                <s style={{ color: '#76777B', marginRight: '8px', fontSize: '16px' }}>
-                  {item.regular_price} Nok
-                </s>
-                <span style={{ color: '#BEEB09', fontSize: '16px' }}>{item.sale_price} Nok</span>
-              </p>
-            ) : (
-              <p style={{ fontSize: '16px' }}>{item.price} Nok</p>
-            )}
-            <button onClick={() => removeFromCart(item.id)}>Remove</button>
-          </div>
-        </div>
+      <div className="cart-wrapper">
+      <div className="cart-content">
+        {cart.map((item) => {
+          const onSale = item.sale_price;
+          return (
+            <div key={item.id}>
+              <div className="row">
+                <img src={item.images[0]?.src} alt={item.images[0]?.alt || 'product'} />
+                <div className="info">
+                  <h2>{item.name}</h2>
+                  <div className="price-and-button">
+                    {onSale ? (
+                      <p>
+                        <s style={{ color: '#76777B', marginRight: '8px', fontSize: '16px' }}>
+                          {item.regular_price} Nok
+                        </s>
+                        <span style={{ color: '#BEEB09', fontSize: '16px' }}>{item.sale_price} Nok</span>
+                      </p>
+                    ) : (
+                      <p style={{ fontSize: '16px' }}>{item.price} Nok</p>
+                    )}
+                    <button onClick={() => removeFromCart(item.id)}>Remove</button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          );
+        })}
       </div>
-    </div>
-  );
-})}
-
-
+      <div className="summary-box">
+        <h2>Summary</h2>
+        <div className="summary-row">
+          <p>Total Price: {totalPrice.toFixed(2)} Nok</p>
+          <p>Discount: {totalDiscount.toFixed(2)} Nok</p>
+          <hr></hr>
+          <p>Total After Discount: {finalPrice.toFixed(2)} Nok</p>
+        </div>
+        <button>Checkout</button>
+      </div>
+    </div>  
     </CartPageContainer>
   );
 };
 
 export default Cart;
+
 
