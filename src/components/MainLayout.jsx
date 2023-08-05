9545211198b8f2e49ae4124db3a5fbbc8116001e
Martin Mroz <mamr@hotmail.no>
2023-08-06 00:07:40 +0200

    change mainLayout to flex

--- src/components/MainLayout.jsx
+++ src/components/MainLayout.jsx
@@ -2,18 +2,26 @@ import React from 'react';
 import { useLocation } from 'react-router-dom';
 import Header from './Header/header';
 import Footer from './Footer/footer';
+import styled from 'styled-components';
+
+const StyledMainLayout = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex: 1 0 auto;
+`;
 
 const MainLayout = ({ children }) => {
   const location = useLocation();
 
   return (
-    <>
+    <StyledMainLayout>
       {location.pathname !== '/' && <Header />}
       {children}
       <Footer />
-    </>
+    </StyledMainLayout>
   );
 };
 
 export default MainLayout;
+
 
