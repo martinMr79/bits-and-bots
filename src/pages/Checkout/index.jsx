8cf65a94946fe175e023b1c6e34792bb11ed99e3
Martin Mroz <mamr@hotmail.no>
2023-08-07 15:53:12 +0200

    adding form

--- src/pages/Checkout/index.jsx
+++ src/pages/Checkout/index.jsx
@@ -1,11 +1,96 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useCart } from '../../hooks/useCart';
+import { useNavigate } from 'react-router-dom';
+import Modal from 'react-modal';
 
 const Checkout = () => {
+  const { cart, clearCart } = useCart();
+  const navigate = useNavigate();
+  const [formData, setFormData] = useState({
+    name: '',
+    address: '',
+    creditCard: ''
+  });
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value
+    }));
+  };
+
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+
+  const handleCheckout = (e) => {
+    e.preventDefault();
+    openModal();
+  };
+
+  const confirmPayment = () => {
+    closeModal();
+    clearCart();
+    navigate("/browse");
+  };
+
   return (
     <div>
-      <h1>Checkout Page</h1>      
+      <h2>You have {cart.length} items in your cart</h2>
+      <form onSubmit={handleCheckout}>
+        <label>
+          Name:
+          <input
+            type="text"
+            name="name"
+            value={formData.name}
+            onChange={handleInputChange}
+            required
+          />
+        </label>
+        <label>
+          Address:
+          <input
+            type="text"
+            name="address"
+            value={formData.address}
+            onChange={handleInputChange}
+            required
+          />
+        </label>
+        <label>
+          Credit Card:
+          <input
+            type="text"
+            name="creditCard"
+            value={formData.creditCard}
+            onChange={handleInputChange}
+            pattern="\d{4} \d{4} \d{4} \d{4}"
+            required
+          />
+        </label>
+        <button type="submit">Submit</button>
+      </form>
+
+      <Modal
+        isOpen={isModalOpen}
+        onRequestClose={closeModal}
+      >
+        <h2>Confirm Payment</h2>
+        <p>Do you want to proceed with the payment?</p>
+        <button onClick={confirmPayment}>Confirm</button>
+        <button onClick={closeModal}>Cancel</button>
+      </Modal>
     </div>
   );
 };
 
 export default Checkout;
+
+
